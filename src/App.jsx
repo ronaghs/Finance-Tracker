bd6ee585904425379b42c8c0405d128169192d8a
Sepehr Ronagh <sepehrronagh@hotmail.com>
2024-09-25 17:36:07 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500 text-6xl flex justify-center items-center h-screen">
+        Finance Tracker
+      </h1>
+    </>
+  );
+}
+
+export default App;
