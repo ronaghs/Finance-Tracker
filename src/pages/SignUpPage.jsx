f2325f21d1bf18d36cc15f1bc571d265a952c702
Sepehr Ronagh <sepehrronagh@hotmail.com>
2024-09-30 11:45:01 -0400

    Introduce a sign up page

---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,17 @@
+function SignupPage() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize: "48px",
+      }}
+    >
+      Sign Up Page
+    </div>
+  );
+}
+
+export default SignupPage;
