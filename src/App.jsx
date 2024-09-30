91bb8586decce6d068cd0e0fcbc60ae8eab7cf6f
Sepehr Ronagh <sepehrronagh@hotmail.com>
2024-09-30 11:34:32 -0400

    Add sign up route

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { Routes, Route } from "react-router-dom";
-import LandingPage from "./LandingPage";
-import LoginPage from "./LoginPage";
-import Dashboard from "./Dashboard";
-import BudgetTracker from "./BudgetTracker";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import Dashboard from "./pages/Dashboard";
+import BudgetTracker from "./components/BudgetTracker";
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/budget-tracker" element={<BudgetTracker />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/budget-tracker" element={<BudgetTracker />} />
+    </Routes>
+  );
 }
 
 export default App;
