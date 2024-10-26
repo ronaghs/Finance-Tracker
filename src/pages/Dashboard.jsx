1b257c4d6b8a86bab460032f92e8cb42066bcc02
Sepehr Ronagh <sepehrronagh@hotmail.com>
2024-10-26 15:21:56 -0400

    Fix month-budget bug and enhance styling

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { doc, deleteDoc, onSnapshot, collection } from "firebase/firestore";
+import { db } from "../firebase/firebaseconfig";
 import ResponsiveAppBar from "../components/ResponsiveAppBar";
 import IncomeEditor from "../components/IncomeEditor";
 import ExpenseEditor from "../components/ExpenseEditor";
@@ -7,7 +9,7 @@ import BudgetCreator from "../components/BudgetCreator";
 import useIncomesAndExpenses from "../hooks/useIncomesAndExpenses";
 import usePopupState from "../hooks/usePopupState";
 import useFinancialData from "../hooks/useFinancialData";
-import useBudgets from "../hooks/useBudgets"; // Import the useBudgets hook
+import { FaEdit, FaTrash } from "react-icons/fa";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -20,7 +22,6 @@ import {
   ArcElement,
 } from "chart.js";
 
-// Register chart components
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -35,18 +36,56 @@ ChartJS.register(
 function Dashboard() {
   const [selected, setSelected] = useState("October 2024");
   const [isBudgetPopupOpen, setBudgetPopupOpen] = useState(false);
-  const { isIncomePopupOpen, isExpensePopupOpen, openIncomeEditor, openExpenseEditor, setIncomePopupOpen, setExpensePopupOpen, editIncome, editExpense } = usePopupState();
-  const { incomes, expenses } = useIncomesAndExpenses(isIncomePopupOpen, isExpensePopupOpen);
-  const budgets = useBudgets(); // Get budgets data
+  const [budgetToEdit, setBudgetToEdit] = useState(null);
+  const [budgets, setBudgets] = useState([]);
 
-  // Calculate account balance
+  const {
+    isIncomePopupOpen,
+    isExpensePopupOpen,
+    openIncomeEditor,
+    openExpenseEditor,
+    setIncomePopupOpen,
+    setExpensePopupOpen,
+    editIncome,
+    editExpense,
+  } = usePopupState();
+
+  const { incomes, expenses } = useIncomesAndExpenses(
+    isIncomePopupOpen,
+    isExpensePopupOpen
+  );
+
+  // Real-time budget updates
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "budgets"),
+      (snapshot) => {
+        const updatedBudgets = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setBudgets(updatedBudgets);
+      },
+      (error) => console.error("Error fetching budgets: ", error)
+    );
+
+    return () => unsubscribe();
+  }, []);
+
   const calculateAccountBalance = () => {
-    const totalIncome = incomes.reduce((acc, income) => acc + Number(income.value), 0);
-    const totalExpenses = expenses.reduce((acc, expense) => acc + Number(expense.value), 0);
+    const totalIncome = incomes.reduce(
+      (acc, income) => acc + Number(income.value),
+      0
+    );
+    const totalExpenses = expenses.reduce(
+      (acc, expense) => acc + Number(expense.value),
+      0
+    );
     return totalIncome - totalExpenses;
   };
 
-  const { months, incomeTotals, expenseTotals, netIncomeTotals } = useFinancialData(incomes, expenses);
+  const { months, incomeTotals, expenseTotals, netIncomeTotals } =
+    useFinancialData(incomes, expenses);
   const accountBalance = calculateAccountBalance();
   const [selectedMonth, selectedYear] = selected.split(" ") || ["", ""];
 
@@ -59,99 +98,159 @@ function Dashboard() {
 
   const filteredExpenses = expenses.filter((expense) => {
     const expenseDate = new Date(expense.date);
-    const expenseMonth = expenseDate.toLocaleString("default", { month: "long" });
+    const expenseMonth = expenseDate.toLocaleString("default", {
+      month: "long",
+    });
     const expenseYear = expenseDate.getFullYear().toString();
     return expenseMonth === selectedMonth && expenseYear === selectedYear;
   });
 
-  // Calculate current spending for each budget category
   const budgetData = budgets.map((budget) => {
-    // Parse budget start and end dates
     const budgetStartDate = new Date(budget.startDate);
     const budgetEndDate = new Date(budget.endDate);
 
-    // Calculate current expenses if the budget type is "expense"
-    const currentExpenses = budget.type === "expense"
-      ? filteredExpenses
-        .filter((expense) => 
-          expense.category === budget.category &&
-          new Date(expense.date) >= budgetStartDate &&
-          new Date(expense.date) <= budgetEndDate
-        )
-        .reduce((acc, expense) => acc + Number(expense.value), 0)
-    : null; // Use null if type is not "expense"
+    const currentExpenses =
+      budget.type === "expense"
+        ? expenses
+            .filter(
+              (expense) =>
+                expense.category === budget.category &&
+                new Date(expense.date) >= budgetStartDate &&
+                new Date(expense.date) <= budgetEndDate
+            )
+            .reduce((acc, expense) => acc + Number(expense.value), 0)
+        : null;
 
-    // Calculate current incomes if the budget type is "income"
-    const currentIncomes = budget.type === "income"
-      ? filteredIncomes
-        .filter((income) => 
-          income.category === budget.category &&
-          new Date(income.date) >= budgetStartDate &&
-          new Date(income.date) <= budgetEndDate
-        )
-        .reduce((acc, income) => acc + Number(income.value), 0)
-    : null; // Use null if type is not "income"
+    const currentIncomes =
+      budget.type === "income"
+        ? incomes
+            .filter(
+              (income) =>
+                income.category === budget.category &&
+                new Date(income.date) >= budgetStartDate &&
+                new Date(income.date) <= budgetEndDate
+            )
+            .reduce((acc, income) => acc + Number(income.value), 0)
+        : null;
 
-    // Create a budget object with only the relevant fields
-    const budgetEntry = {
+    return {
+      id: budget.id,
       category: budget.category,
       budgetValue: budget.value,
+      budgetStartDate: budget.startDate,
+      budgetEndDate: budget.endDate,
+      currentSpending: currentExpenses,
+      currentIncome: currentIncomes,
     };
-
-    // Add currentSpending or currentIncome to the budget entry if they are not null
-    if (currentExpenses !== null) {
-      budgetEntry.currentSpending = currentExpenses;
-    }
-
-    if (currentIncomes !== null) {
-      budgetEntry.currentIncome = currentIncomes;
-    }
-
-    return budgetEntry;
   });
+
+  const handleEditBudget = (budget) => {
+    setBudgetToEdit(budget);
+    setBudgetPopupOpen(true);
+  };
+
+  const handleDeleteBudget = async (budgetId) => {
+    try {
+      await deleteDoc(doc(db, "budgets", budgetId));
+    } catch (error) {
+      console.error("Error deleting budget: ", error);
+    }
+  };
 
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
       <ResponsiveAppBar />
-
       <div className="container mx-auto">
         <h2 className="text-3xl font-bold mb-6 text-gray-800">Dashboard</h2>
 
         <div className="mb-6">
-          <h3 className="text-xl font-semibold text-gray-800">Account Balance</h3>
-          <p className={`text-2xl font-bold ${accountBalance >= 0 ? "text-green-600" : "text-red-600"}`}>
+          <h3 className="text-xl font-semibold text-gray-800">
+            Account Balance
+          </h3>
+          <p
+            className={`text-2xl font-bold ${
+              accountBalance >= 0 ? "text-green-600" : "text-red-600"
+            }`}
+          >
             ${accountBalance.toFixed(2)}
           </p>
         </div>
 
         <div className="flex flex-col md:flex-row items-center mb-6">
-          <label htmlFor="month-selector" className="mb-2 md:mb-0 mr-4 text-lg font-medium text-gray-700">Select Month:</label>
-          <select id="month-selector" value={selected} onChange={(e) => setSelected(e.target.value)} className="border border-gray-300 rounded-lg p-2 text-lg w-full md:w-auto">
+          <label
+            htmlFor="month-selector"
+            className="mb-2 md:mb-0 mr-4 text-lg font-medium text-gray-700"
+          >
+            Select Month:
+          </label>
+          <select
+            id="month-selector"
+            value={selected}
+            onChange={(e) => setSelected(e.target.value)}
+            className="border border-gray-300 rounded-lg p-2 text-lg w-full md:w-auto"
+          >
             {months.map((month) => (
-              <option key={month} value={month}>{month}</option>
+              <option key={month} value={month}>
+                {month}
+              </option>
             ))}
           </select>
         </div>
 
-        {/* Display budget information */}
         <div className="mt-8">
-        <h3 className="text-xl font-semibold text-gray-800 mb-4">Budgets</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {budgetData.map((budget) => (
-            <div key={budget.category} className="bg-white p-4 shadow rounded-lg">
-              <h4 className="text-lg font-semibold text-blue-600">{budget.category}</h4>
-              <p className="text-sm text-gray-600">Budget: ${budget.budgetValue}</p>
-              {/* Conditionally render current spending if it exists */}
-              {budget.currentSpending !== undefined && (
-                <p className="text-sm text-gray-600">Current Spending: ${budget.currentSpending.toFixed(2)}</p>
-              )}
-              {/* Conditionally render current income if it exists */}
-              {budget.currentIncome !== undefined && (
-                <p className="text-sm text-gray-600">Current Income: ${budget.currentIncome.toFixed(2)}</p>
-              )}
-            </div>
-          ))}
-        </div>
+          <h3 className="text-xl font-semibold text-gray-800 mb-4">Budgets</h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {budgetData.map((budget) => (
+              <div
+                key={budget.id}
+                className="bg-white p-6 shadow-lg rounded-xl transition-transform transform hover:scale-105 hover:shadow-xl relative"
+              >
+                <div className="flex justify-between items-center">
+                  <h4 className="text-lg font-semibold text-blue-600">
+                    {budget.category}
+                  </h4>
+                  <div className="flex space-x-3">
+                    <FaEdit
+                      onClick={() => handleEditBudget(budget)}
+                      className="text-blue-600 cursor-pointer hover:text-blue-800"
+                      title="Edit Budget"
+                    />
+                    <FaTrash
+                      onClick={() => handleDeleteBudget(budget.id)}
+                      className="text-red-600 cursor-pointer hover:text-red-800"
+                      title="Delete Budget"
+                    />
+                  </div>
+                </div>
+                <div className="mt-3 text-sm text-gray-600">
+                  <p className="mb-1">
+                    <span className="font-medium">Budget:</span> $
+                    {budget.budgetValue}
+                  </p>
+                  {budget.currentSpending !== null && (
+                    <p className="mb-1">
+                      <span className="font-medium">Current Spending:</span> $
+                      {budget.currentSpending.toFixed(2)}
+                    </p>
+                  )}
+                  {budget.currentIncome !== null && (
+                    <p className="mb-1">
+                      <span className="font-medium">Current Income:</span> $
+                      {budget.currentIncome.toFixed(2)}
+                    </p>
+                  )}
+                  <p className="mb-1">
+                    <span className="font-medium">Start Date:</span>{" "}
+                    {budget.budgetStartDate}
+                  </p>
+                  <p>
+                    <span className="font-medium">End Date:</span>{" "}
+                    {budget.budgetEndDate}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
 
         <FinancialChart
@@ -160,7 +259,7 @@ function Dashboard() {
           expenseTotals={expenseTotals}
           netIncomeTotals={netIncomeTotals}
           selectedMonth={selectedMonth}
-          selectedIncome={filteredIncomes} 
+          selectedIncome={filteredIncomes}
           selectedExpenses={filteredExpenses}
         />
 
@@ -186,31 +285,36 @@ function Dashboard() {
             </button>
           </div>
 
+          {/* Income Cards */}
           <div className="mt-6">
             <h3 className="text-xl font-semibold text-gray-800 mb-4">
               Incomes
             </h3>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
               {filteredIncomes.map((income) => (
-                <div key={income.id} className="bg-white p-4 shadow rounded-lg">
+                <div
+                  key={income.id}
+                  className="bg-white p-6 shadow-lg rounded-lg relative border border-blue-100 hover:border-blue-300 transition duration-200 ease-in-out transform hover:scale-105"
+                >
                   <h4 className="text-lg font-semibold text-blue-600">
                     {income.name}
                   </h4>
-                  <p className="text-sm text-gray-600">{income.date}</p>
-                  <p className="text-lg font-bold text-gray-800">
+                  <p className="text-sm text-gray-500">{income.date}</p>
+                  <p className="text-xl font-bold text-gray-800 mt-2">
                     ${income.value}
                   </p>
                   <button
                     onClick={() => openIncomeEditor(income)}
-                    className="mt-4 bg-yellow-500 hover:bg-yellow-600 text-white py-1 px-3 rounded-lg"
+                    className="mt-4 text-blue-500 hover:text-blue-700 font-medium flex items-center"
                   >
-                    Edit
+                    <i className="fas fa-pen mr-2"></i> Edit
                   </button>
                 </div>
               ))}
             </div>
           </div>
 
+          {/* Expense Cards */}
           <div className="mt-8">
             <h3 className="text-xl font-semibold text-gray-800 mb-4">
               Expenses
@@ -219,20 +323,20 @@ function Dashboard() {
               {filteredExpenses.map((expense) => (
                 <div
                   key={expense.id}
-                  className="bg-white p-4 shadow rounded-lg"
+                  className="bg-white p-6 shadow-lg rounded-lg relative border border-red-100 hover:border-red-300 transition duration-200 ease-in-out transform hover:scale-105"
                 >
                   <h4 className="text-lg font-semibold text-red-600">
                     {expense.name}
                   </h4>
-                  <p className="text-sm text-gray-600">{expense.date}</p>
-                  <p className="text-lg font-bold text-gray-800">
+                  <p className="text-sm text-gray-500">{expense.date}</p>
+                  <p className="text-xl font-bold text-gray-800 mt-2">
                     ${expense.value}
                   </p>
                   <button
                     onClick={() => openExpenseEditor(expense)}
-                    className="mt-4 bg-yellow-500 hover:bg-yellow-600 text-white py-1 px-3 rounded-lg"
+                    className="mt-4 text-red-500 hover:text-red-700 font-medium flex items-center"
                   >
-                    Edit
+                    <i className="fas fa-pen mr-2"></i> Edit
                   </button>
                 </div>
               ))}
@@ -272,16 +376,26 @@ function Dashboard() {
               </div>
             </div>
           )}
+
           {isBudgetPopupOpen && (
             <div className="popup fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
               <div className="popup-content bg-white p-6 rounded-lg shadow-lg relative w-full max-w-md">
                 <button
-                  onClick={() => setBudgetPopupOpen(false)}
+                  onClick={() => {
+                    setBudgetPopupOpen(false);
+                    setBudgetToEdit(null);
+                  }}
                   className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
                 >
                   &times;
                 </button>
-                <BudgetCreator onClose={() => setBudgetPopupOpen(false)} />
+                <BudgetCreator
+                  onClose={() => {
+                    setBudgetPopupOpen(false);
+                    setBudgetToEdit(null);
+                  }}
+                  budget={budgetToEdit}
+                />
               </div>
             </div>
           )}
